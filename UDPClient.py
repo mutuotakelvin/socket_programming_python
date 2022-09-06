f8f4605b1d9405fa357059c45faff239a5f249cc
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-09-07 00:50:43 +0300

    Addes a sending functionality

--- UDPClient.py
+++ UDPClient.py
@@ -1,6 +1,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print('The OS assigned the address {} to me'.format(s.getsockname()))
-message = input('Input lowercase sentence:')
+message = input('Input a lowercase sentence:')
 data = message.encode('ascii')
+s.sendto(data,('127.0.0.1',3000))
+print('The OS assigned the address {} to me'.format(s.getsockname()))
