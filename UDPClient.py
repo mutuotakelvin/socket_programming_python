5094203ccb239c2466a846bb7266cfebd340a89f
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-09-07 00:55:55 +0300

    Addea receiving functionality

--- UDPClient.py
+++ UDPClient.py
@@ -1,7 +1,10 @@
 import socket
 
+MAX_SIZE_BYTES = 65535 # Maximu size of a UDP datagram
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 message = input('Input a lowercase sentence:')
 data = message.encode('ascii')
 s.sendto(data,('127.0.0.1',3000))
 print('The OS assigned the address {} to me'.format(s.getsockname()))
+data, address = s.recvfrom(MAX_SIZE_BYTES)
