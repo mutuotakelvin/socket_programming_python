f23f2afe3ec12f10d9649cbbd43db4406b0a6a79
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-09-07 00:31:07 +0300

    added a receiving message functionality

--- UDPServer.py
+++ UDPServer.py
@@ -1,4 +1,7 @@
 import socket
+
+MAX_SIZE_BYTES = 65535
+
 # Setting up a socket
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 port = 3000
@@ -6,3 +9,6 @@ hostname = '127.0.0.1'
 # Binding the socket to a port and IP address
 s.bind((hostname,port))
 print('Listening at {}'.format(s.getsockname()))
+
+while True:
+    data, clientAddress = s.recvfrom(MAX_SIZE_BYTES) # Receive at most 65535 bytes at once
